48eb6071fa24d08e675a5524d4cea18f3f6bfa12
Amarjeet kumar <126227902+amarjeetcse@users.noreply.github.com>
2023-12-25 11:06:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const quoteText = document.querySelector(".quote"),
+quoteBtn = document.querySelector("button"),
+authorName = document.querySelector(".name"),
+speechBtn = document.querySelector(".speech"),
+copyBtn = document.querySelector(".copy"),
+twitterBtn = document.querySelector(".twitter"),
+whatsappBtn = document.querySelector(".whatsapp"),
+synth = speechSynthesis;
+
+function randomQuote(){
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading Quote..."; 
+    fetch("https://api.quotable.io/random").then(response => response.json()).then(result => {
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.classList.remove("loading");
+        quoteBtn.innerText = "New Quote";
+    });
+}
+
+speechBtn.addEventListener("click", ()=>{
+    if(!quoteBtn.classList.contains("loading")){
+        let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+        synth.speak(utterance);
+        setInterval(()=>{
+            !synth.speaking ? speechBtn.classList.remove("active") : speechBtn.classList.add("active");
+        }, 10);
+    }
+});
+
+copyBtn.addEventListener("click", ()=>{
+    navigator.clipboard.writeText(quoteText.innerText);
+});
+
+twitterBtn.addEventListener("click", ()=>{
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+    window.open(tweetUrl, "_blank");
+});
+whatsappBtn.addEventListener("click", ()=>{
+    let whatsappUrl = `https://whatsapp.com/intent/whatsapp?url=${quoteText.innerText}`;
+    window.open(whatsappUrl , "_blank");
+});
+
+
+quoteBtn.addEventListener("click", randomQuote);
